f14be328cb041f3a7d0f44b4cd396100382e647b
seungmanchoi <blueng.choi@gmail.com>
2026-03-01 05:20:37 +0900

    chore: bump version to 1.5.1

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ export class AppPublisherServer {
   constructor() {
     this.server = new McpServer({
       name: 'app-publisher-mcp',
-      version: '1.5.0',
+      version: '1.5.1',
     });
 
     this.setupTools();
